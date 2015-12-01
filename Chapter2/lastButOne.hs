5737d2aa8407b32f501a404828394d747b8d625a
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-01 23:48:59 +0000

    Defined lastButOne fucntion which returns second last element of a list.

--- Chapter2/lastButOne.hs
+++ Chapter2/lastButOne.hs
@@ -1,6 +1,6 @@
 -- file  ch02/lastButOne.hs
 
 --lastButOne :: ([a] -> a)
-lastButOne xs = if (drop 2 xs) ==[]
+lastButOne xs = if (drop 2 xs) ==[] && xs /= []
                 then head xs
                 else lastButOne (tail xs)
