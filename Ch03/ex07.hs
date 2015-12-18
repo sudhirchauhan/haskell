764fca028fedd9e0e3e1b41e91e534bc828bf6a0
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2015-12-18 15:45:31 +0000

    auto-commit: Fri Dec 18 15:45:31 UTC 2015

--- Ch03/ex07.hs
+++ Ch03/ex07.hs
@@ -3,6 +3,6 @@
 intersperseList :: a -> [[a]] -> [a]
 
 intersperseList sep [] = []
-intersperseList sep [[x]] = [x]
-intersperseList sep xs = head xs  ++ [sep] ++ intersperseList sep (tail xs)
+i--intersperseList sep (x:[]) = [x]
+intersperseList sep x:xs = xs  ++ [sep] ++ intersperseList sep (tail xs)
 
