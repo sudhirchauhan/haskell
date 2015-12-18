7c7620b3e2a3566b51ceee0e8ef399a9592cb084
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2015-12-18 13:44:22 +0000

    auto-commit: Fri Dec 18 13:44:22 UTC 2015

--- Ch03/ex05.hs
+++ Ch03/ex05.hs
@@ -4,6 +4,6 @@ isPalindrome :: [a] -> Bool
 isPalindrome [] = True
 
 isPalindrome (x:[]) = True
-isPalindrome (x:y:_:z:a:[])
-  | x == a = isPalindrome(y:_:z)
+isPalindrome xs
+  | head xs == last xs  = isPalindrome(y:_:z)
   | otherwise = False
