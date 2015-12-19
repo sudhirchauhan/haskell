841e1e89d2e0b2c39753fea33ba2875064f59d87
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 07:28:20 +0000

    auto-commit: Sat Dec 19 07:28:20 GMT 2015

--- Ch03/ex09.hs
+++ Ch03/ex09.hs
@@ -6,3 +6,15 @@ data Point = Point Integer Integer deriving (Show,Eq)
 data Direction = Direction Left
                  | Right
                  | Straight
+
+getDirection :: Point a -> Point a -> Point a -> Direction
+
+-- if xa == xb == xc then stright
+-- if xa < xb > xc then left else Right
+-- if xa > xb < xc then Left else Right 
+getDirection (Point xa ya)  (Point xb yb) (Point xc yc)
+  | (xa == xb == xb) = Straight
+  | (xa < xb > xc)   = Left
+  | (xa > xb < xc)   = Left
+  | otherwise        = Right
+
