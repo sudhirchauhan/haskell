3e38125dbd9683f42c21ca15adf62e460e0b8a40
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 07:12:31 +0000

    auto-commit: Sat Dec 19 07:12:31 GMT 2015

--- Ch03/ex09.hs
+++ Ch03/ex09.hs
@@ -1,4 +1,8 @@
 -- Directions data type
 -- Given three points return left or right depending upon the angle abc formed between ab and bc
 
-data Point = (X ::Integer , Y:Integer)
+data Point = Point Integer Integer deriving (Show,Eq)
+
+data Direction = Direction Left
+                 | Right
+                 | Straight
