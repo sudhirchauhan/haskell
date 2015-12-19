f934f3d743591b16ab537a124ae5312936dd7de1
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 08:19:32 +0000

    auto-commit: Sat Dec 19 08:19:32 GMT 2015

--- Ch03/ex09.hs
+++ Ch03/ex09.hs
@@ -22,8 +22,8 @@ getDirection :: Point -> Point  -> Point -> Direction
 -- -- if xa == xb == xc then stright
 -- -- if xa < xb > xc then left else Right
 -- -- if xa > xb < xc then GoLeft else Right 
-getDirection a  b c
-  | (getSlope a b) == (getSlope b c)  = GoStraight
- -- | (getSlope a b) < (getSlope b c)   = GoLeft
- -- | (getSlope a b) >
+getDirection (Point xa ya)  (Point xb yb) (Point xc yc)
+  | (xa == xb) == (xb == xc)  = GoStraight
+  | (xa < xb)   = GoLeft
+  | (xa > xb)   = GoLeft
   | otherwise   = GoRight
