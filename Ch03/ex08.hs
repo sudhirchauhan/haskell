398c823bd1e0a9c433fd8169329e6219cbe745ee
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 06:15:29 +0000

    auto-commit: Sat Dec 19 06:15:29 GMT 2015

--- Ch03/ex08.hs
+++ Ch03/ex08.hs
@@ -11,8 +11,9 @@ treeHeight (Node x Empty Empty)  = 1
 treeHeight (Node _ left Empty)   = 1 + treeHeight left
 treeHeight (Node _ Empty right)  = 1 + treeHeight right
 
-treeHeight (Node x left right)  = let lh = treeHeight left
-                                     rh = treeHeight right
-                                  in 
- 
+treeHeight (Node x left right)  = let lh = 1 + treeHeight left
+                                      rh = 1 + treeHeight right
+                                  in case (lh <= rh) of
+                                         True -> rh
+                                         _    -> lh
 
