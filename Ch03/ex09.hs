604fc2320137239fe5c615ae5b57a1cd4e540f95
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 08:00:05 +0000

    auto-commit: Sat Dec 19 08:00:05 GMT 2015

--- Ch03/ex09.hs
+++ Ch03/ex09.hs
@@ -12,8 +12,8 @@ data Direction = GoLeft | GoRight | GoStraight
                deriving(Show)
                    
 
-getSlope :: Point -> Point -> Integer
-getSlope (Point xa ya) (Point xb yb) = (yb -ya)/(xb -xa)
+--getSlope :: Point -> Point -> Integer
+--getSlope (Point xa ya) (Point xb yb) = (yb -ya)/(xb -xa)
 
 
 
@@ -23,7 +23,7 @@ getDirection :: Point -> Point  -> Point -> Direction
 -- -- if xa < xb > xc then left else Right
 -- -- if xa > xb < xc then GoLeft else Right 
 getDirection (Point xa ya)  (Point xb yb) (Point xc yc)
-  | (xa == xb)  = GoStraight
+  | (xa == xb) == (xb == xc)  = GoStraight
   | (xa < xb)   = GoLeft
   | (xa > xb)   = GoLeft
   | otherwise   = GoRight
