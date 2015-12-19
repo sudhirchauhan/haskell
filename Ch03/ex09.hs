c609b33ee9a220a1444feac70f9aca17cdc8d27a
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 07:51:04 +0000

    auto-commit: Sat Dec 19 07:51:04 GMT 2015

--- Ch03/ex09.hs
+++ Ch03/ex09.hs
@@ -18,7 +18,7 @@ getDirection :: Point -> Point  -> Point -> Direction
 -- -- if xa < xb > xc then left else Right
 -- -- if xa > xb < xc then GoLeft else Right 
 getDirection (Point xa ya)  (Point xb yb) (Point xc yc)
-  | (xa == xb)  = GoStraight
+  | (xa == xb) && (xb == xc)  = GoStraight
   | (xa < xb)   = GoLeft
   | (xa > xb)   = GoLeft
   | otherwise   = GoRight
