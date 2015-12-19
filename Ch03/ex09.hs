3f1311de81e2c6e477f44b4a263b737354baf69d
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 07:37:25 +0000

    auto-commit: Sat Dec 19 07:37:25 GMT 2015

--- Ch03/ex09.hs
+++ Ch03/ex09.hs
@@ -3,6 +3,11 @@
 
 data Point = Point Integer Integer deriving (Show,Eq)
 
+
+type TurnLeft   = Integer
+type TurnRight  = Integer
+type GoStraight = Integer
+
 data Direction = Direction TurnLeft
                  | TurnRight
                  | GoStraight
