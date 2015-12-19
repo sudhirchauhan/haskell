1eff351dad9605f2ae05ac1c4dfc17f32a508511
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 05:32:13 +0000

    auto-commit: Sat Dec 19 05:32:13 GMT 2015

--- Ch03/ex08.hs
+++ Ch03/ex08.hs
@@ -6,7 +6,7 @@ data Tree a = Node a (Tree a) (Tree a)
 
 -- Read the height of tree
 treeHeight :: Tree a -> Integer
-treeHeight x Empty = 0
-treeHeight x Empty Empty = 1
+treeHeight (Empty) = 0
+treeHeight (x Empty Empty) = 1
 --treeHeight Node x (Tree x) Empty = 1 + (treeHeight Tree x)
 
