ab43f45168a65698bf3641ce1c521f0ec2c671db
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 06:07:31 +0000

    auto-commit: Sat Dec 19 06:07:31 GMT 2015

--- Ch03/ex08.hs
+++ Ch03/ex08.hs
@@ -7,8 +7,10 @@ data Tree a = Node a (Tree a) (Tree a)
 -- Read the height of tree
 treeHeight :: Tree a -> Integer
 treeHeight (Empty) = 0
-treeHeight (Node x Empty Empty) = 1
-treeHeight (Node _ left Empty)  = 
+treeHeight (Node x Empty Empty)  = 1
+treeHeight (Node _ left Empty)   = 1 + treeHeight left
+treeHeight (Node _ Empty right)  = 1 + treeHeight right
+
 treeHeight (Node x left right)  = let lh = treeHeight left
                                      rh = treeHeight right
                                   in 
