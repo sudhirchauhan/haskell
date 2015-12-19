76054305928a418ac1d6c4a5390714c621727e33
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-19 07:31:45 +0000

    auto-commit: Sat Dec 19 07:31:45 GMT 2015

--- Ch03/ex09.hs
+++ Ch03/ex09.hs
@@ -7,14 +7,13 @@ data Direction = Direction Left
                  | Right
                  | Straight
 
-getDirection :: Point a -> Point a -> Point a -> Direction
+-- getDirection :: Point a -> Point a -> Point a -> Direction
 
--- if xa == xb == xc then stright
--- if xa < xb > xc then left else Right
--- if xa > xb < xc then Left else Right 
-getDirection (Point xa ya)  (Point xb yb) (Point xc yc)
-  | (xa == xb == xb) = Straight
-  | (xa < xb > xc)   = Left
-  | (xa > xb < xc)   = Left
-  | otherwise        = Right
-
+-- -- if xa == xb == xc then stright
+-- -- if xa < xb > xc then left else Right
+-- -- if xa > xb < xc then Left else Right 
+-- getDirection (Point xa ya)  (Point xb yb) (Point xc yc)
+--   | (xa == xb == xb) = Straight
+--   | (xa < xb > xc)   = Left
+--   | (xa > xb < xc)   = Left
+--   | otherwise        = Right
