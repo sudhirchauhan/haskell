14b5cd512deb0c630b88167751f1e96daf8c3f02
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-20 21:49:58 +0000

    auto-commit: Sun Dec 20 21:49:58 GMT 2015

--- Ch04/SplitLines.hs
+++ Ch04/SplitLines.hs
@@ -3,8 +3,8 @@ splitLines :: String -> [String]
 
 splitLines  [] = []
 splitLines cs  =
-let (pre, suf) = break isLineTerminator cs
-  in pre: case suf of
+ let (pre, suf) = break isLineTerminator cs
+ in pre: case suf of
    ('\r':'\n':rest) -> splitLines rest
    ('\r':rest)      -> splitLines rest
    ('\n':rest)      -> splitLines rest
