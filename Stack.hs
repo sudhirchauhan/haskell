a09e9cccd4db6d04285db154187ba26d0bc6f303
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2015-12-23 11:31:34 +0000

    auto-commit: Wed Dec 23 11:31:34 UTC 2015

--- Stack.hs
+++ Stack.hs
@@ -3,6 +3,10 @@ module Stack (Stack, insert,swap) where
 
 type Stack w = [w]
 
-insert :: w-> Stack w
+--insert :: w-> Stack w
 
 swap :: Stack w -> Stack w
+
+swap [] = []
+swap [w] = [w]
+swap (w1:w2:ws) = w2 : w1 : ws
