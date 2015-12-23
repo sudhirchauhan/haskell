f52ae6df07d830f6eaef96b3b23d610fc212a065
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2015-12-23 11:37:42 +0000

    auto-commit: Wed Dec 23 11:37:42 UTC 2015

--- Stack.hs
+++ Stack.hs
@@ -5,8 +5,12 @@ type Stack w = [w]
 
 --insert :: w-> Stack w
 
-swap :: Stack w -> Stack w
 
+swap :: Stack w -> Stack w
 swap [] = []
 swap [w] = [w]
 swap (w1:w2:ws) = w2 : w1 : ws
+
+
+focusNext :: Stack w -> Stack w
+focusNext []  = []
