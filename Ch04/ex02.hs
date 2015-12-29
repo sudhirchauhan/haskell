c32591e5164f40e67ad17a987ac9ca73181405db
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2015-12-29 15:52:32 +0000

    auto-commit: Tue Dec 29 15:52:32 UTC 2015

--- Ch04/ex02.hs
+++ Ch04/ex02.hs
@@ -1,7 +1,12 @@
-
-
 splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith _ []        = []
+splitWith f (x:xs)    =
+  let acc     = []
+      p f acc (x:xs) = case f x of
+                          |False -> [[acc ++ [x]] ++  p f acc  xs
+                          |_     -> p f (acc ++ [x]) xs
+  in p f  [] (x:xs)
+  
+  
 
 
-splitWith _ []      = []
-splitWith p (x:xs)  = let  
