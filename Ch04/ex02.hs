ee7abfae094a8ad288bb0ed42b6d32df988c6f6b
Sudhir Chauhan <sudhirsinghchauhan@gmail.com>
2015-12-29 06:22:11 +0000

    auto-commit: Tue Dec 29 06:22:11 GMT 2015

--- Ch04/ex02.hs
+++ Ch04/ex02.hs
@@ -6,4 +6,3 @@ splitWith :: (a -> Bool) -> [a] -> [[a]]
 splitWith _ []      = []
 splitWith f (x:xs)  = case f x of
                          False -> [ [x] ++ splitWith f xs]
-			 -     ->[x] ++  splitwith f xs
