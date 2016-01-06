6dc87bf2675f65c503522cf298b62bb766d78f12
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2016-01-06 15:16:14 +0000

    auto-commit: Wed Jan  6 15:16:14 UTC 2016

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -1,3 +1,10 @@
 module PrettyJSON (renderJValue) where
 
-renderJValue :: JValue 
+renderJValue :: JValue -> Doc
+
+renderJValue (JBool True)  = text "true"
+renderJValue (JBool False) = text "false"
+renderJValue JNull         = text "null"
+renderJValue (JBool True)  = text "true"
+renderJValue (JBool True)  = text "true"
+
