aa0e4ad7f83f3b6884d2ae3ca18e9a12ae7770dd
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2016-01-06 14:17:43 +0000

    auto-commit: Wed Jan  6 14:17:43 UTC 2016

--- ch05/PutJSON.hs
+++ ch05/PutJSON.hs
@@ -2,3 +2,8 @@ import Data.List(intercalate)
 import SimpleJSON
 
 renderJValue :: JValue -> String
+
+renderJValue (JString s) = show s
+renderJValue (JNumber n) = show n
+renderJValue (JBool True)= "true"
+renderJValue (JBool False)= "false"
