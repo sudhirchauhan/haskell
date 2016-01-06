58ede10988df11353abdc2c7e4101a30e587709c
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2016-01-06 14:24:47 +0000

    auto-commit: Wed Jan  6 14:24:47 UTC 2016

--- ch05/PutJSON.hs
+++ ch05/PutJSON.hs
@@ -1,9 +1,19 @@
 import Data.List(intercalate)
 import SimpleJSON
 
+
 renderJValue :: JValue -> String
 
 renderJValue (JString s) = show s
 renderJValue (JNumber n) = show n
 renderJValue (JBool True)= "true"
 renderJValue (JBool False)= "false"
+renderJValue JNull   = "null"
+
+renderJValue (JObject o) = "{" ++ pairs o ++ "}"
+ where pairs [] = ""
+       pairs ps = intercalate "," (map renderPair ps)
+       renderPair (k,v)   = show k ++ ":" ++ renderJValue v
+
+renderJValue (JArray a) = "[" ++ values a ++ "]"
+
