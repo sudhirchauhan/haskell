eea51ee7ae7ae4d17e884a219c4bc4a971d74ff0
Sudhir Singh Chauhan <SudhirSinghChauhan@gmail.com>
2016-01-06 15:20:42 +0000

    auto-commit: Wed Jan  6 15:20:42 UTC 2016

---
+++ ch05/Prettify.hs
@@ -0,0 +1,12 @@
+import SimpleJSON
+
+data Doc = Doc deriving(Show)
+
+string :: String -> Doc
+String s = undefined
+
+text :: String -> Doc
+text s = undefined
+
+double :: Double -> Doc
+double n = undefined
